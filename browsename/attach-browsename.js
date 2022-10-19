10961e9d34e17132370d57f6411aea1219c1d49c
yinjilong <yinjilong@gmail.com>
2022-10-19 11:14:39 +0900

    also attach browseName msg which topic is nodeId

--- browsename/attach-browsename.js
+++ browsename/attach-browsename.js
@@ -5,39 +5,32 @@ module.exports = function (RED) {
         node.browse_names = {};
         node.id_token = /ns=\d?;[is]=/
         node.on('input', function (msg) {
-            if (msg.hasOwnProperty('browseName')) {
-                var topic = msg.topic;
-                if (node.id_token.test(msg.topic)) {
+            if (typeof msg.topic === 'string' && node.id_token.test(msg.topic)) {
+                var nodeId = msg.topic;
+                if (msg.hasOwnProperty('browseName')) {
                     // add to dict
                     var browseName = msg.browseName;
-                    node.browse_names[topic] = browseName;
+                    node.browse_names[nodeId] = browseName;
                     if (config.enableStatus)
                         this.status({ fill: "green", shape: "dot", text: "browseName stored:" + browseName });
-                }
-            }
-            else {
-                if (typeof msg.topic === 'object') {
-                    if (msg.topic.hasOwnProperty('nodeId')) {
-                        var nodeID = msg.topic.nodeId;
-                        //attach browse_name
-                        if (node.browse_names.hasOwnProperty(nodeID) && !msg.topic.hasOwnProperty('browseName')) {
-                            msg.topic.browseName = node.browse_names[nodeID];
-                            if (config.enableStatus)
-                                this.status({ fill: "green", shape: "dot", text: "browseName attached:" + msg.topic.browseName });
-                        }
-                    }
-                } else if (node.id_token.test(msg.topic)) {  //topic is ua node number
-                    var nodeID = msg.topic.nodeId;
-                    if (node.browse_names.hasOwnProperty(nodeID) && !msg.hasOwnProperty('browseName')) {
-                        msg.browseName = node.browse_names[nodeID];
-                        if (config.enableStatus)
-                            this.status({ fill: "green", shape: "dot", text: "browseName attached:" + msg.browseName });
-                    }
 
+                } else if (node.browse_names.hasOwnProperty(nodeId)) {
+                    msg.browseName = node.browse_names[nodeId];
                 }
-                node.send(msg);
+            } else if (typeof msg.topic === 'object') {
+                if (msg.topic.hasOwnProperty('nodeId') && !msg.topic.hasOwnProperty('browseName')) {
+                    var nodeId = msg.topic.nodeId;
+                    if (node.browse_names.hasOwnProperty(nodeId)) {
+                        //attach browse_name                    
+                        msg.topic.browseName = node.browse_names[nodeId];
+                        if (config.enableStatus)
+                            this.status({ fill: "green", shape: "dot", text: "browseName attached:" + msg.topic.browseName });
+                    }
+                }
             }
-        });
+            if (msg.hasOwnProperty(`serverTimestamp`))
+            node.send(msg);
+        })
     }
     RED.nodes.registerType("attach-node-browseName", AttachNodeBrowseName);
 }
